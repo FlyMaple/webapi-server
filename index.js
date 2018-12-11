b4ce6712abf8fccaa5f29ff0f587d19e2229adf2
FlyMaple <rottensociety1987@gmail.com>
2018-12-11 23:28:52 +0800

    update
    Signed-off-by: FlyMaple <rottensociety1987@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 
-const heroes = [
+let heroId = 21;
+
+let heroes = [
     { id: 11, name: 'Mr. Nice' },
     { id: 12, name: 'Narco' },
     { id: 13, name: 'Bombasto' },
@@ -22,9 +24,14 @@ app.use(cors({
     credentials: false,
 }));
 
-// app.use(bodyParse({
-//     type: ''
-// }));
+app.use(bodyParser.json());
+
+const findHeroByName = function (name) {
+    return heroes.find(hero => hero.name === name);
+};
+const findHeroById = function (id) {
+    return heroes.find(hero => hero.id === id);
+};
 
 app.get('/api/heroes', (req, res) => {
     const term = (req.query.q || '').toLowerCase();
@@ -42,6 +49,37 @@ app.get('/api/hero/:heroId', (req, res) => {
     const heroId = +req.params.heroId;
 
     res.json(heroes.find(hero => hero.id === heroId));
+});
+
+app.post('/api/hero', (req, res) => {
+    const name = req.body.name;
+
+    if (findHeroByName(name)) {
+        res.json({});
+    } else {
+        const hero = {
+            id: ++heroId,
+            name
+        };
+        heroes.push(hero);
+        res.json(hero);
+    }
+});
+
+app.put('/api/hero/:heroId', (req, res) => {
+    const heroId = +req.params.heroId;
+    const newHeroName = req.body.name;
+    const matchHero = findHeroById(heroId);
+    if (matchHero) {
+        matchHero.name = newHeroName;
+    } 
+    res.json({});
+});
+
+app.delete('/api/hero/:heroId', (req, res) => {
+    const heroId = +req.params.heroId;
+    heroes = heroes.filter(hero => hero.id !== heroId);
+    res.json({});
 });
 
 app.listen(3000, () => {
